598f4116915a712e44c07f43941def5b3aba6229
Carolyn Wang <carolynwang@college.harvard.edu>
2021-03-26 16:29:31 -0400

    now you can make a forest!!

--- src/canvas.js
+++ src/canvas.js
@@ -1,5 +1,6 @@
 import React, {useRef, useEffect, useState} from 'react'
-import getPosition from './getPosition.js'
+// import draw from './draw'
+
 
 const Canvas = props => {
 
@@ -9,50 +10,54 @@ const Canvas = props => {
     const [y, setY] = useState(140)
 
     function getClickPosition(e) {
-        const position = getPosition(e.currentTarget, e.clientX, e.clientY)
-        const tempX = position.x - 144
-        const tempY = position.y -150
+        
+
+        console.log(e.current)
+
+        const tempX = e.clientX
+        const tempY = e.clientY
+
+        // initiates re-render so tree is drawn at new x,y
         setX(tempX);
-        setY(tempY);    
+        setY(tempY); 
+        
+        
+        
+
+        const color = {
+            red: 255,
+            green: getRandomInt(80) + 50,
+            blue: getRandomInt(50) + 100,
+        }
+        
+        draw(tempX,tempY, 100, 40, 20, 10, color)
+        draw(tempX,tempY, 100, 0, 20, 10, color)
+        draw(tempX,tempY, 100, -40, 20, 10, color)
     }
 
-    // const draw = ctx => {
-    //     // random
-
-
-    //     ctx.fillStyle = '#ff5c85'
-    //     ctx.beginPath()
-
-    //     console.log(ctx.beginPath())
-        
-    //     for (let i=0; i<6;i++) {
-    //         ctx.arc(x, y-(i*10), 10, 0, 2*Math.PI)
-    //     }
-    //     ctx.fill()
-    // }
-
-    function getRandomInt(max) {
+    // random integer function
+    const getRandomInt = (max) => {
         return Math.ceil(Math.random() * Math.ceil(max));
     }
-
-    const draw = (startX, startY, len, angle, branchThickness, likelihood, ctx) => {
-
+    
+    // draw function!
+    const draw = (startX, startY, len, angle, branchThickness, likelihood, color) => {
+        const ctx = canvasRef.current.getContext('2d')
         console.log('draw called')
-        // console.log('inputs',startX, startY, len, angle, ctx, ctx.beginPath())
-
+    
         
-
-        // constants in order of use
+    
+        // constants 
         const radius = branchThickness/2
-        const color = '#ff4f72'
+        const coralColor = `rgb(${color.red},${color.green},${color.blue})`
         const render = getRandomInt(likelihood)
         const side = getRandomInt(3)
-        const end = getRandomInt(3)
-
+        
+    
         if (branchThickness === 20) {
             ctx.beginPath()
             ctx.save();
-            ctx.fillStyle = '#ff5c85'
+            ctx.fillStyle = coralColor
             
             ctx.translate(startX, startY);
             ctx.rotate(angle * Math.PI/180);
@@ -64,7 +69,7 @@ const Canvas = props => {
         }
         
         
-
+    
         // draw branch, draws curved branches at the base and straight ones at the top
         ctx.beginPath()
         ctx.save();
@@ -80,26 +85,27 @@ const Canvas = props => {
                 ctx.bezierCurveTo(-10, -len/2, -10, -len/2, 0, -len);
             }
         }
-        ctx.strokeStyle = color
+        ctx.strokeStyle = coralColor
         ctx.lineWidth = branchThickness
         ctx.stroke();
-
+    
         ctx.restore()
-
+    
         ctx.beginPath()
         ctx.save();
-        ctx.fillStyle = color
+        ctx.fillStyle = coralColor
         
         ctx.translate(startX, startY);
         ctx.rotate(angle * Math.PI/180);
-
+    
         ctx.arc(0,0-(len*.95),radius,0,Math.PI,true)
         
         ctx.fill()
-
-
-
-        
+    
+    
+        const end = getRandomInt(3)
+    
+        // stops generating if end === 2 and branch is less than 10 px thick
         if (end > 1) {
             if(branchThickness < 10) {
                 // makes ends rounded
@@ -116,65 +122,48 @@ const Canvas = props => {
                 ctx.restore();
                   return;
             }
-
+    
         }
-
-        if(branchThickness < 9) {
-            // makes ends rounded
+    
+        // always shops generating if branch thickness is less than four
+        if(branchThickness < 4) {
+    
+            // draws light pink circles on the ends
             ctx.restore();
             ctx.beginPath()
             ctx.save();
-            ctx.fillStyle = '#ffbac8'
-            
+            ctx.fillStyle = '#ffbac8';
             ctx.translate(startX, startY);
             ctx.rotate(angle * Math.PI/180);
-
-            ctx.arc(0,0-(len*.95),radius,0,Math.PI,true)
-            ctx.fill()
+            ctx.arc(0,0-(len*.95),radius,0,Math.PI,true);
+            ctx.fill();
             ctx.restore();
-              return;
+            return;
         }
-
-        // if (branchThickness === 20) {
-        //     branchThickness = 15
-        // } else {
-        //     branchThickness = 0.8*branchThickness
-        // }
-
+    
+        // randomly generates angles at which branches protrude
         const ang1 = getRandomInt(30) + 20
         const ang2 = getRandomInt(30) + 20
-
-        let newLen;
-        if (branchThickness > 4 && branchThickness < 6) {
-            newLen = branchThickness*0.5
+    
+        // makes branches progressively shorter, change to make it look more tree-like!
+        const newLen = len*0.5
+    
+        // randomizes if the branches formed are left, right, or both
+        if (side === 0) {
+            draw(0, -len, newLen, -ang2, branchThickness*0.8, likelihood*1.5, color);
+        } else if (side === 1) {
+            draw(0, -len, newLen, ang1, branchThickness*0.8, likelihood*1.5, color);
         } else {
-            newLen = branchThickness*0.8
-        }
-
+            draw(0, -len, newLen, -ang2, branchThickness*0.8, likelihood*1.5, color);
+            draw(0, -len, newLen, ang1, branchThickness*0.8, likelihood*1.5, color);
+        }    
+    
+        // draws a straight branch ALWAYS
+        draw(0, -len, len*0.6, 0, branchThickness*0.8, likelihood*1.5, color);
         
-
-        
-
-        // randomizes what direction the branches go it
-        // if (render > 5) {
-            if (side === 0) {
-                draw(0, -len, newLen, -ang2, branchThickness*0.8, likelihood*1.5,ctx);
-            } else if (side === 1) {
-                draw(0, -len, newLen, ang1, branchThickness*0.8, likelihood*1.5, ctx);
-            } else {
-                draw(0, -len, newLen, -ang2, branchThickness*0.8, likelihood*1.5, ctx);
-                draw(0, -len, newLen, ang1, branchThickness*0.8, likelihood*1.5, ctx);
-            }
-            
-        // }
-        
-        draw(0, -len, len*0.6, 0, branchThickness*0.8, likelihood*1.5, ctx);
-        
-        
+        // restores context before next call
         ctx.restore();
     }
-
-    
 
     // so that getContext isn't called until after component has mounted
     useEffect(() => {
@@ -192,11 +181,9 @@ const Canvas = props => {
         }
 
 
-        draw(x,y, 100, 40, 20, 10, context)
-        draw(x,y, 100, 0, 20, 10,context)
-        draw(x,y, 100, -40, 20, 10, context)
+        
         // draw(context)
-    },[draw])
+    },[])
 
     console.log('rendered')
 
@@ -204,3 +191,20 @@ const Canvas = props => {
 }
 
 export default Canvas
+
+
+// practice draw function
+// const draw = ctx => {
+//     // random
+
+
+//     ctx.fillStyle = '#ff5c85'
+//     ctx.beginPath()
+
+//     console.log(ctx.beginPath())
+    
+//     for (let i=0; i<6;i++) {
+//         ctx.arc(x, y-(i*10), 10, 0, 2*Math.PI)
+//     }
+//     ctx.fill()
+// }
